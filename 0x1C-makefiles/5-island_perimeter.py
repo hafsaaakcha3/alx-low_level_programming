fbc6e7704406e92c65d1460aeae469f1a1242f89
hafsaaakcha3 <hafsaaakcha3@gmail.com>
2024-02-24 17:11:21 -0800

    0

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" technical interview task """
+
+
+def island_perimeter(grid):
+    """ calculates the perimeter of the island """
+    p = 0
+    flag = 0
+    for j in range(len(grid)):
+        for i in range(len(grid[j])):
+            if grid[j][i] == 1:
+                flag = 1
+                if (j == 0) or (i == 0):
+                    p += 2
+                elif not (
+                    grid[j-1][i] == 1 and
+                    grid[j][i-1] == 1 and
+                    grid[j-1][i-1] == 1
+                ):
+                    p += 2
+    if flag == 1:
+        p += 2
+    return (p)
