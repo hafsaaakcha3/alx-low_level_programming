e956af1dff41d259c8ae67a766c7715b11532cd0
hafsaaakcha3 <hafsaaakcha3@gmail.com>
2024-04-14 17:44:41 +0000

    0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+/* Function prototype for linear search */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t beguin, size_t end);
+#endif /* SEARCH_ALGOS_H */
